6b019c3fcd13807db74cc4c4fdbcccc09efc91c8
Mahesh Holkar <151976187+MaheshH91@users.noreply.github.com>
2026-06-16 18:03:21 +0530

    Added two demo code files

---
+++ demo-two.js
@@ -0,0 +1,8 @@
+console.log("Mahesh Holkar");
+console.log('Mahesh Holkar')
+console.log(9)
+console.log(true)
+console.log(9,'Mahesh','19',"Holkar")
+process.stdout.write("Hello, World!\n");
+process.stdout.write("Mahesh Holkar");
+process.stdout.write('Mahesh Holkar')
